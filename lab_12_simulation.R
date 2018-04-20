aac7079214bc63455c91dd742c107af462320e9f
Matthew Yudin <matyudin@Matthews-Air.wv.cc.cmu.edu>
2018-04-20 00:54:41 -0400

    Extracts p-value from regression

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -3,3 +3,15 @@ generate_data = function(n,p){
   responses = rnorm(n)
   return(list(covariates,responses))
 }
+
+model_select = function(covariates,responses,cutoff){
+  cov.lm = lm(responses ~ covariates)
+  p.values = summary(cov.lm)$coefficients[,4]
+  if (length(covariates[covariates<=cutoff])==0){
+    return(c())
+  }
+  cov.lm.2 = lm(responses ~ covariates[,covariates<=cutoff])
+  p.values.2 = summary(cov.lm.2)$coefficients[,4]
+  return(summary(cov.lm.2))
+}
+
