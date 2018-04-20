49aaac6310955c5a82a154a86c99ffca14703868
Matthew Yudin <matyudin@Matthews-Air.wv.cc.cmu.edu>
2018-04-20 01:50:24 -0400

    Runs simulations of model_select.

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -12,6 +12,19 @@ model_select = function(covariates,responses,cutoff){
   }
   cov.lm.2 = lm(responses ~ covariates[,covariates<=cutoff])
   p.values.2 = summary(cov.lm.2)$coefficients[,4]
-  return(summary(cov.lm.2))
+  return(p.values.2)
+}
+
+run_simulation = function(n_trial=1, n, p, cutoff=0.05){
+  n=c(100,1000,10000)
+  p=c(10,20,50)
+  for (i in 1:length(n_trials)){
+    for (j in 1:3){
+      for (k in 1:3){
+        ml = model_select(generate_data(n[j],p[k])$covariates,generate_data(n[j],p[k])$responses,cutoff)
+        hist(ml)
+      }
+    }
+  }
 }
 
